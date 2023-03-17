a8baeca5b186bcb0eb48f3754451cde91a9ef2dd
Niko Fendi Lienata <37220554+nikofendi@users.noreply.github.com>
2023-03-18 02:43:33 +0700

    tambah file js

--- skripkode.js
+++ skripkode.js
@@ -1,4 +1,12 @@
-     <script>
-        function myFunction()  { var x = document.getElementById ("myNavbar")}
+<script>
+	 /*  Toggle between showing and hiding the   nav     igation menu links when the user clicks on the      hamburger menu icon */
+        function myFunction() {
+	 		var x = document.getElementById("myNavbar");
+        if (x.className === "navbar") {
+                x.className += " responsive";
+			} else {
+                x.className = "navbar";
+			}
+		}
+</script>
 
-0
